32956444ea384f530ae86917b655f005ea26bcae
YounesFetouaki <younesfetouaki65@gmail.com>
2025-12-11 00:30:06 +0100

    Update App Branding to StoreManager

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Atelier 1',
+      title: 'StoreManager API',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: ProductListScreen(),
       routes: {
